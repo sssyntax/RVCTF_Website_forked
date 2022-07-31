ce5727128a5fe9c938d93a62c875e9c5dfc5e603
Rayner3103 <80162238+Rayner3103@users.noreply.github.com>
2022-07-31 16:43:18 +0800

    Add Challenge

--- Challenge Page/static/script.js
+++ Challenge Page/static/script.js
@@ -3,8 +3,15 @@
 var chal_popups = document.getElementsByClassName("popup");
 var chal_btns = document.getElementsByClassName("challenge_btn");
 var span = document.getElementsByClassName("close");
+var add_chal_box = document.getElementById("add_chal_box");
+var add_chal_close = document.getElementById('add_chal_close')
+
+add_chal_close.onclick = function(){
+  add_chal_box.style.display = 'none';
+}
 
 for (let i=0; i<chal_popups.length; i++) {
+  console.log(chal_popups[i])
   chal_btns[i].onclick = function() {
     chal_popups[i].style.display = "block";
   }
@@ -16,4 +23,13 @@ for (let i=0; i<chal_popups.length; i++) {
       chal_popups[i].style.display = "none";
     }
   });
+}
+
+function addChal(){
+  add_chal_box.style.display = "block";
+}
+
+function submitChal(){
+  // RDev ppl can do the add in new challenges here!
+  add_chal_box.style.display = "none";
 }
