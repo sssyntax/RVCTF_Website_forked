036c1a17383c3f0e7a8236ad328218429c8770e3
RVCTF <108906514+RVCTF@users.noreply.github.com>
2022-11-21 23:09:39 +0800

    Add files via upload

---
+++ templates/Login Pages/create_team_msg.tpl.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+    <title>RVCTF Create Team</title>
+    <link rel="stylesheet" href="static/css/register.css">
+    <link rel="icon" type="image/x-icon" href="../../static/images/favicon.ico">
+</head>
+<body>
+    <div class="header">Create Team</div>
+    <div style="height: 600px;">
+    <table class="centre_box">
+            <tr>
+                <td>Team has already been created. Do request for the password from the team leader <?php if (isset($_GET['teamleader'])){echo "(".$_GET['teamleader'].")";}?> and join the team</td>
+            </tr>
+            <tr>
+                <td>
+                    <a href = "./index.php?filename=teamjoin">
+                        <button class="button" type="submit">Join Team</button>
+                    </a>
+                </td>
+            </tr>
+    </table>
+    </div>
+</body>
+</html>
---
+++ templates/Login Pages/team_choice.tpl.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<head>
+    <title>RVCTF Team Choice</title>
+    <link rel="stylesheet" href="static/css/register.css">
+    <link rel="icon" type="image/x-icon" href="../../static/images/favicon.ico">
+</head>
+<body>
+    <div class="header">Do you have a team?</div>
+    <div style="height: 600px;">
+    <table class="centre_box">  
+        <tr>
+            <td>
+                <a href = "./index.php?filename=teamcreation">
+                <button id = "teamcreate" value = "Create Team">Create Team</button>
+                </a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href = "./index.php?filename=teamjoin">
+                <button id = "teamjoin" value = "Join Team">Join Team</button>
+                </a>
+            </td>
+        </tr>
+    </table>
+    </div>
+</body>
+</html>
