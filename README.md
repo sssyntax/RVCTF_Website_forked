79a8c1ea2c6649a235844aadc54f031ae6a967c8
NgQiHeng <ng_qi_heng_2@students.edu.sg>
2022-11-21 21:39:30 +0800

    update stuff lmao

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # RVCTF_Website
 GitHub for RVCTF Website
 
-For new devs, please import ctfdb.sql into your xampp and run the mysql module in xampp when running the site
+## For creating the database
+1. Run the MYSQL tab on xampp
+2. import ctfdb.sql into your xampp and run the mysql module in xampp when running the site
+3. Change the username, password, dbname and server name in connect.inc.php
 
 ALL paths should be relative to index.php
 A tags should use href = "index.php?filename=%" where % is the name of the page you wanna go to
+
+
 
 > A collaboration between RVCTF and RdeV
--- backend/includes/connect.inc.php
+++ backend/includes/connect.inc.php
@@ -1,13 +1,14 @@
 <?php
 //THIS NEEDS TO CHANGE IF YOU ARE USING SERVER
-$servername = "localhost"; // for default: 127.0.0.1 
-$username = "pjjabycm_ctfdb"; // for x10: pjjabycm_ctfdb | for others: root
-$password = "q6sFckv3"; // for x10: q6sFckv3 
-$db_name = "pjjabycm_ctfdb"; // for x10: pjjabycm_ctfdb 
-// $servername = "127.0.0.1"; // for default: 127.0.0.1 
-// $username = "root"; // for x10: pjjabycm_ctfdb | for others: root
-// $password = ""; // for x10: q6sFckv3 
-// $db_name = "ctfdb"; // for x10: pjjabycm_ctfdb 
+// $servername = "localhost"; // for default: 127.0.0.1 
+// $username = "pjjabycm_ctfdb"; // for x10: pjjabycm_ctfdb | for others: root
+// $password = "q6sFckv3"; // for x10: q6sFckv3 
+// $db_name = "pjjabycm_ctfdb"; // for x10: pjjabycm_ctfdb 
+// TO EDIT LOCALLY, UNCOMMENT THIS
+$servername = "127.0.0.1"; // for default: 127.0.0.1 
+$username = "root"; // for x10: pjjabycm_ctfdb | for others: root
+$password = ""; // for x10: q6sFckv3 
+$db_name = "ctfdb"; // for x10: pjjabycm_ctfdb 
 $conn = new mysqli($servername,$username,$password,$db_name);
 function prepared_query($mysqli, $sql, $params, $types = "")
 {
