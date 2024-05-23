77a17fd31b5dd9c0e50b0abf7cc26c4dc51e2dba
Zhongbob <89343697+Zhongbob@users.noreply.github.com>
2024-05-23 20:37:53 +0800

    databases

--- backend/databases/ctfdb.sql
+++ backend/databases/ctfdb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2023 at 09:45 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Generation Time: May 23, 2024 at 02:36 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `additional_materials`
+--
+
+CREATE TABLE `additional_materials` (
+  `material_id` int(11) NOT NULL,
+  `challenge_id` int(11) NOT NULL,
+  `file_name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `challenges`
 --
 
@@ -38,28 +50,6 @@ CREATE TABLE `challenges` (
   `solution` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `challenges`
---
-
-INSERT INTO `challenges` (`id`, `title`, `author`, `points`, `difficulty`, `category`, `description`, `solution`) VALUES
-(1, 'testing', 'zhongbob', 69, 1, 'Forensics', 'I am in deep levels of pain', '123'),
-(2, '1', '1', 1, 1, '1', '1', '96e173e55c7eb1edbe72b0a436915190db675d8a'),
-(3, 'a', 'a', 60, 0, 'forensics', 'a', '02cf13ba6ad8abcb048e531de70fee0ae6a5bdff'),
-(4, 'testing', 'zhongbob', 69, 2, 'Forensics', 'I am in deep levels of pain', '123'),
-(5, 'testing', 'zhongbob', 69, 1, 'Forensics', 'I am in deep levels of pain', '123'),
-(6, 'aa', 'a', 69, 0, 'Forensics', 'aadada', 'a34e9cf8da7548eedba9bb52708cc84fc2e2f221'),
-(7, 'dada', 'dada', 69, 0, 'Forensics', 'dadadada', '4b51b18fa511de0f2457ee5a2476b96c10613167'),
-(8, 'dada', 'dada', 69, 0, 'Forensics', 'adada', 'c92236ae0de8f9b75a09a8ab9f653070f924be3b'),
-(9, 'a', 'a', 69, 0, 'Forensics', 'a', 'a7633fdcef488104c60365429f3561ce7835450f'),
-(10, 'a', 'a', 60, 0, 'Forensics', 'da', 'a7633fdcef488104c60365429f3561ce7835450f'),
-(11, 'a', 'a', 60, 0, 'Forensics', 'dadadada', 'a7633fdcef488104c60365429f3561ce7835450f'),
-(12, 'a', 'a', 69, 0, 'Forensics', 'd', 'a7633fdcef488104c60365429f3561ce7835450f'),
-(13, '1', '2', 3, 0, 'Forensics', '4', '3da6f1b21872868b0acc10611250a223a0fc5423'),
-(14, 'your mom', 'me', 69, 0, 'Forensics', 'fuck you', 'b3d68079cfb8c8d3bee8575bd172b290e843711b'),
-(15, '你妈妈', 'me', 69, 0, 'PWN', '', '79af5839c42aa970923018f5a1c2b5bad56234d9'),
-(16, 'JP', 'Me', 69, 0, 'Reverse Engineering', 'The answer is JP', '846400caf36e29558d2fb4cafdf1ccc706917b5c');
-
 -- --------------------------------------------------------
 
 --
@@ -67,19 +57,11 @@ INSERT INTO `challenges` (`id`, `title`, `author`, `points`, `difficulty`, `cate
 --
 
 CREATE TABLE `completedchallenges` (
-  `completionid` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
-  `challengeid` int(11) NOT NULL,
+  `completion_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `challenge_id` int(11) NOT NULL,
   `timestamp` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `completedchallenges`
---
-
-INSERT INTO `completedchallenges` (`completionid`, `userid`, `challengeid`, `timestamp`) VALUES
-(64, 23, 15, 1660581136),
-(69, 23, 16, 1660583423);
 
 -- --------------------------------------------------------
 
@@ -90,39 +72,33 @@ INSERT INTO `completedchallenges` (`completionid`, `userid`, `challengeid`, `tim
 CREATE TABLE `ctf_users` (
   `id` int(11) NOT NULL,
   `email` varchar(320) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `teamname` varchar(255) DEFAULT NULL,
-  `points` int(11) NOT NULL DEFAULT 0,
-  `admin` tinyint(1) NOT NULL
+  `username` text NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `ctf_users`
---
-
-INSERT INTO `ctf_users` (`id`, `email`, `password`, `teamname`, `points`, `admin`) VALUES
-(2, 'chua_zhong_ding@students.edu.sg', '', NULL, 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `loginattempts`
+-- Table structure for table `pending_invite`
 --
 
-CREATE TABLE `loginattempts` (
+CREATE TABLE `pending_invite` (
   `id` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
-  `timestamp` int(11) NOT NULL
+  `user_email` varchar(512) NOT NULL,
+  `team_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `loginattempts`
+-- Table structure for table `teamates`
 --
 
-INSERT INTO `loginattempts` (`id`, `userid`, `timestamp`) VALUES
-(3, 0, 1659767693),
-(4, 0, 1659767701),
-(5, 0, 1659767763);
+CREATE TABLE `teamates` (
+  `teamate_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `team_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -131,22 +107,10 @@ INSERT INTO `loginattempts` (`id`, `userid`, `timestamp`) VALUES
 --
 
 CREATE TABLE `teams` (
-  `teamid` int(11) NOT NULL,
-  `teamname` varchar(255) NOT NULL,
-  `teampassword` varchar(255) NOT NULL,
-  `teammates` text NOT NULL,
-  `teamleader` varchar(320) NOT NULL,
-  `points` int(11) NOT NULL DEFAULT 0
+  `team_id` int(11) NOT NULL,
+  `team_name` varchar(255) NOT NULL,
+  `teamleader_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `teams`
---
-
-INSERT INTO `teams` (`teamid`, `teamname`, `teampassword`, `teammates`, `teamleader`, `points`) VALUES
-(3, 'Zhongbob Team', '$2y$10$zRf9uDI3SfaOEnqW/18oneqN18Ph.AP9cOVV2HQSPBI6vkyvFlYAK', '[\"czhongding@gmail.com\"]', 'czhongding@gmail.com', 0),
-(11, 'ilyJP', '$2y$10$.xgdzMHLtcQp5YkjUgnH/.JtjhayjoWnzigsrAEHI9dglnGV3JjG.', '[\"nqheng@gmail.com\",\"ngqiheng952@gmail.com\"]', 'nqheng@gmail.com', 0),
-(12, 'test@gmail.com', '$2y$10$9gfoJBTTE3sBQvxou5rd7.8jFnpSIx5znSOnRY2SlrUehWqN3OPG2', '[\"test@gmail.com\"]', 'test@gmail.com', 0);
 
 -- --------------------------------------------------------
 
@@ -155,24 +119,34 @@ INSERT INTO `teams` (`teamid`, `teamname`, `teampassword`, `teammates`, `teamlea
 --
 
 CREATE TABLE `tokens` (
-  `tokenid` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
+  `token_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `token` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tokens`
---
-
-INSERT INTO `tokens` (`tokenid`, `userid`, `token`) VALUES
-(6, 1, '611fc082ed4dc98feabcb3e18c5f5e8b2e1989fba9591e472e05b62e34eb536c'),
-(7, 2, 'f45f3d941682634470cd3c647da3e95d8256d7b01d5dd6e2050768849cb4c6a5'),
-(8, 2, '6fef23e981d16183d72fe48c53a56e04c0d13703061b18184abb090d3ce0196a'),
-(9, 2, 'd1669abf8fab6337a867c2fe889d98f20bff132546b2e17b5a35d3a5e8f09fba');
-
---
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `additional_materials`
+--
+ALTER TABLE `additional_materials`
+  ADD PRIMARY KEY (`material_id`),
+  ADD KEY `challenge_id` (`challenge_id`);
+
+--
+-- Indexes for table `challenges`
+--
+ALTER TABLE `challenges`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `completedchallenges`
+--
+ALTER TABLE `completedchallenges`
+  ADD PRIMARY KEY (`completion_id`),
+  ADD KEY `completedchallenges_ibfk_1` (`challenge_id`);
 
 --
 -- Indexes for table `ctf_users`
@@ -182,26 +156,127 @@ ALTER TABLE `ctf_users`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `pending_invite`
+--
+ALTER TABLE `pending_invite`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_email` (`user_email`,`team_id`),
+  ADD KEY `pending_invite_ibfk_1` (`team_id`);
+
+--
+-- Indexes for table `teamates`
+--
+ALTER TABLE `teamates`
+  ADD PRIMARY KEY (`teamate_id`),
+  ADD KEY `teamates_ibfk_1` (`team_id`),
+  ADD KEY `teamates_ibfk_2` (`user_id`);
+
+--
+-- Indexes for table `teams`
+--
+ALTER TABLE `teams`
+  ADD PRIMARY KEY (`team_id`),
+  ADD KEY `teamleader_id` (`teamleader_id`);
+
+--
 -- Indexes for table `tokens`
 --
 ALTER TABLE `tokens`
-  ADD PRIMARY KEY (`tokenid`);
+  ADD PRIMARY KEY (`token_id`),
+  ADD KEY `tokens_ibfk_1` (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `additional_materials`
+--
+ALTER TABLE `additional_materials`
+  MODIFY `material_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `challenges`
+--
+ALTER TABLE `challenges`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `completedchallenges`
+--
+ALTER TABLE `completedchallenges`
+  MODIFY `completion_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `ctf_users`
 --
 ALTER TABLE `ctf_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pending_invite`
+--
+ALTER TABLE `pending_invite`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `teamates`
+--
+ALTER TABLE `teamates`
+  MODIFY `teamate_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `teams`
+--
+ALTER TABLE `teams`
+  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tokens`
 --
 ALTER TABLE `tokens`
-  MODIFY `tokenid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `token_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `additional_materials`
+--
+ALTER TABLE `additional_materials`
+  ADD CONSTRAINT `additional_materials_ibfk_1` FOREIGN KEY (`challenge_id`) REFERENCES `challenges` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `completedchallenges`
+--
+ALTER TABLE `completedchallenges`
+  ADD CONSTRAINT `completedchallenges_ibfk_1` FOREIGN KEY (`challenge_id`) REFERENCES `challenges` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pending_invite`
+--
+ALTER TABLE `pending_invite`
+  ADD CONSTRAINT `pending_invite_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `teams` (`team_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `teamates`
+--
+ALTER TABLE `teamates`
+  ADD CONSTRAINT `teamates_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `teams` (`team_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `teamates_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `ctf_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `teams`
+--
+ALTER TABLE `teams`
+  ADD CONSTRAINT `teams_ibfk_1` FOREIGN KEY (`teamleader_id`) REFERENCES `ctf_users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tokens`
+--
+ALTER TABLE `tokens`
+  ADD CONSTRAINT `tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `ctf_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
