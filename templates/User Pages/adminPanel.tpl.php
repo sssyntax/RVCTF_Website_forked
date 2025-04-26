ee61c43745331b4975716ead7d3f6f3ef901ad99
RVCTF <108906514+RVCTF@users.noreply.github.com>
2025-04-26 17:27:42 +0800

    Update adminPanel.tpl.php
    FIXED BUTTON HITBOXES

--- templates/User Pages/adminPanel.tpl.php
+++ templates/User Pages/adminPanel.tpl.php
@@ -1,14 +1,15 @@
+
+
 <title>Admin Panel</title>
-    <link rel="stylesheet" href="static/css/makeadmin.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="static/css/makeadmin.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
 </head>
 <body>
-    <a href = "index.php?filename=editadmin" >
-        <input type = "button" class = "button" value = "Edit Admin" style = "width:fit-content;margin:auto;display:block;">
-    </a><br>
-    <a href = "index.php?filename=createChallenge">
-        <input type = "button" class = "button" value = "Create Challenge" style = "width:fit-content;margin:auto;display:block;"> 
-    </a>
+    <?php include 'templates/Components/stars.php';?>
+    <div id="admin-buttons">
+        <a href="index.php?filename=editadmin" class="admin-button">Edit Admin</a>
+        <a href="index.php?filename=createChallenge" class="admin-button">Create Challenge</a>
+    </div>
 </body>
