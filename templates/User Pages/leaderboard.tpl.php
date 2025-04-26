034b9ef6c4bc8bee8db59b25ee549371570a7401
RVCTF <108906514+RVCTF@users.noreply.github.com>
2025-04-26 21:56:29 +0800

    Update leaderboard.tpl.php
    fixing leaderboard

--- templates/User Pages/leaderboard.tpl.php
+++ templates/User Pages/leaderboard.tpl.php
@@ -1,72 +1,72 @@
-    <title>Leaderboard</title>
-    <link rel='stylesheet' href='static/css/leaderboard.css'>
+<title>Leaderboard</title>
+<link rel='stylesheet' href='static/css/leaderboard.css'>
 </head>
 
 <body>
   <?php include 'backend/leaderboard.php'?>
-<?php include 'templates/Components/stars.php';?>
-<h2 style = "text-align:center;">Team Ranks</h2>
-      <table class="table team">
-        <tr>
-          <td>
-            <ul class="leader">
-                  <?php 
-                  $index = 1;
-                  foreach($teams as $team){ ?>
-                    <li id = 
-                    <?php if ($index == 1) {
-                      echo "decoration";
-                      } else if ($index == 2) {
-                        echo "decoration2";
-                      } else if ($index == 3) {
-                        echo "decoration3";
-                      } ?>>
-                        <span class='list_num'><?php echo $index; ?></span>
-                        <h2>
-                          <?php 
-                          // Only get the front part of the email as username
-                          echo $team['team_name']; ?>
-                          <span class="number">
-                            <?php echo $team['points']; ?>
-                          </span>
-                        </h2>
-                    </li>
-                  <?php $index++;}?>
-            </ul>
-          </td>
-        </tr>
-      </table>
-      <h2 style = "text-align:center;">Individual Ranks</h2>
-      <table class="table individual">
-        <tr>
-          <td>
-            <ul class="leader">
-                  <?php 
-                  $index = 1;
-                  foreach($users as $user){ ?>
-                    <li id = 
-                    <?php if ($index == 1) {
-                      echo "decoration";
-                      } else if ($index == 2) {
-                        echo "decoration2";
-                      } else if ($index == 3) {
-                        echo "decoration3";
-                      } ?>>
-                        <span class='list_num'><?php echo $index; ?></span>
-                        <h2>
-                          <?php 
-                          // Only get the front part of the email as username
-                          echo $user['username']; ?>(<?= $user['team_name'] ?>)
-                          <span class="number">
-                            <?php echo $user['points']; ?>
-                          </span>
-                        </h2>
-                    </li>
-                  <?php $index++;}?>
-            </ul>
-          </td>
-        </tr>
-      </table>
+  <?php include 'templates/Components/stars.php';?>
+
+  <h2 id="team-ranks-header">Team Ranks</h2>
+  <table class="table team">
+    <tr>
+      <td>
+        <ul class="leader">
+          <?php 
+          $index = 1;
+          foreach($teams as $team){ ?>
+            <li id=
+            <?php if ($index == 1) {
+              echo "decoration";
+            } else if ($index == 2) {
+              echo "decoration2";
+            } else if ($index == 3) {
+              echo "decoration3";
+            } ?>>
+              <span class='list_num'><?php echo $index; ?></span>
+              <h2>
+                <?php 
+                echo $team['team_name']; ?>
+                <span class="number">
+                  <?php echo $team['points']; ?>
+                </span>
+              </h2>
+            </li>
+          <?php $index++; } ?>
+        </ul>
+      </td>
+    </tr>
+  </table>
+
+  <h2 id="individual-ranks-header">Individual Ranks</h2>
+  <table class="table individual">
+    <tr>
+      <td>
+        <ul class="leader">
+          <?php 
+          $index = 1;
+          foreach($users as $user){ ?>
+            <li id=
+            <?php if ($index == 1) {
+              echo "decoration";
+            } else if ($index == 2) {
+              echo "decoration2";
+            } else if ($index == 3) {
+              echo "decoration3";
+            } ?>>
+              <span class='list_num'><?php echo $index; ?></span>
+              <h2>
+                <?php 
+                echo $user['username']; ?> (<?= $user['team_name'] ?>)
+                <span class="number">
+                  <?php echo $user['points']; ?>
+                </span>
+              </h2>
+            </li>
+          <?php $index++; } ?>
+        </ul>
+      </td>
+    </tr>
+  </table>
 
 </body>
 </html>
